520f2801c86ec2769d6f3f0eb813a387b6d1073f
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-10-06 15:49:22 +0100

    add about page and help page

---
+++ train_model.py
@@ -0,0 +1,8 @@
+from model import CreateModelFacade
+
+
+feature_columns = ['EC Combined (mpg)', 'Fuel', 'Mileage', 'Standard Euro Emissions', 'Transmission']
+label_column = 'Price'
+
+create_model_facade = CreateModelFacade('https://www.cargiant.co.uk', feature_columns, label_column, 20)
+model = create_model_facade.get_model()
