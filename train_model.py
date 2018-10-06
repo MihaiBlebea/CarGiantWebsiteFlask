6b0e9ccae9b8ea9425025c6aa19d105fbb233700
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-10-06 17:02:52 +0100

    add model and excel file

--- train_model.py
+++ train_model.py
@@ -4,5 +4,5 @@ from model import CreateModelFacade
 feature_columns = ['EC Combined (mpg)', 'Fuel', 'Mileage', 'Standard Euro Emissions', 'Transmission']
 label_column = 'Price'
 
-create_model_facade = CreateModelFacade('https://www.cargiant.co.uk', feature_columns, label_column, 20)
+create_model_facade = CreateModelFacade('https://www.cargiant.co.uk', feature_columns, label_column, 2)
 model = create_model_facade.get_model()
