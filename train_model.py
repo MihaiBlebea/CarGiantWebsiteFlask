7fdb0999bdba0efb1c1fb1e99d127a579205f617
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-10-11 13:30:27 +0100

    delete test file

--- train_model.py
+++
@@ -1,8 +0,0 @@
-from model import CreateModelFacade
-
-
-feature_columns = ['EC Combined (mpg)', 'Fuel', 'Mileage', 'Standard Euro Emissions', 'Transmission']
-label_column = 'Price'
-
-create_model_facade = CreateModelFacade('https://www.cargiant.co.uk', feature_columns, label_column, 2)
-model = create_model_facade.get_model()
